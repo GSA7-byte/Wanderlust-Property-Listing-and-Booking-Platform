714989a1d0d3da7b1dec75335ff07b2e1da227ca
Gaurav Singh Adhikari <2306201@kiit.ac.in>
2026-02-28 12:17:37 +0530

    Fix route handler to catch all requests

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-app.all("*splat", (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
 
