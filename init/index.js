2c0f1616da52892a623608dda3431b6ae544f1c9
Gaurav Singh Adhikari <2306201@kiit.ac.in>
2026-02-28 12:53:36 +0530

    Use environment variable for MongoDB URL

--- init/index.js
+++ init/index.js
@@ -4,7 +4,7 @@ const Listing = require("../models/listing.js");
 const { init } = require("../models/user.js");
 const { object } = require("joi");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
   .then(() => {
