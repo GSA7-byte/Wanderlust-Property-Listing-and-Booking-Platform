65a97cfb34e16f452646e057d15dc009e4a15b6b
GSA7-byte <2306201@kiit.ac.in>
2026-03-05 19:05:40 +0530

    Deployment commit2

--- init/index.js
+++ init/index.js
@@ -1,10 +1,11 @@
+require("dotenv").config({ path: "../.env" });
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 const { init } = require("../models/user.js");
 const { object } = require("joi");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
